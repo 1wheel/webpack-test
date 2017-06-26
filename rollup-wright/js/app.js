01e716f32a5211145796941a0c36fbc548aeac7b
Adam Pearce <1wheel@gmail.com>
2017-06-26 18:31:40 -0400

    wright

---
+++ rollup-wright/js/app.js
@@ -0,0 +1,8 @@
+import reload from './component.js'
+
+window.reload = reload
+
+window.onload = function() {
+  document.body.innerText = 'Refreshed at ' + new Date()
+  console.log('reloadddsdfasdfsdfsdfsdfddd')
+}
